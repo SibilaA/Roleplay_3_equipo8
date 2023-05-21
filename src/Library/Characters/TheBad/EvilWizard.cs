94e4e4ecbbc30ec63c5e9887a353aa0c7a4722f6
TadeoKi <tadeo.ki@correo.ucu.edu.uy>
2023-05-21 11:52:25 -0300

    correccion de la clase evilwizard

--- src/Library/Characters/TheBad/EvilWizard.cs
+++ src/Library/Characters/TheBad/EvilWizard.cs
@@ -2,16 +2,28 @@ using System.Collections.Generic;
 
 namespace RoleplayGame
 {
-    public class EvilArcher : AVillains
-    {
-        public EvilArcher(string name)
+    public class EvilWizard : AVillains , IMagicCharacter
+    {   
+        private List<IMagicalItem> magicalItem = new List<IMagicalItem>();
+        public EvilWizard(string name)
         {
             this.Name = name;
             this.health = 100;
             
-            this.AddItem(new Bow());
-            this.AddItem(new Helmet());
-            this.VictoryPoints = 1;
+            this.AddItem(new magicRobe());
+            this.AddItem(new magicScepter());
+            
         }
+
+        public void AddItem(IMagicalItem item)
+        {
+            this.magicalItem.Add(item);
+        }
+
+        public void RemoveItem(IMagicalItem item)
+        {
+            this.magicalItem.Remove(item);
+        }    
+
     }
 }
